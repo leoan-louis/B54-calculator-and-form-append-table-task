45481424a028c18852e943d57faea697315dc90f
leoan <142498946+leoan-louis@users.noreply.github.com>
2023-12-22 10:44:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let displayValue = "";
+
+function addToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    document.getElementById("display").value = result;
+    displayValue = result.toString();
+  } catch (error) {
+    alert("Invalid expression!");
+    clearDisplay();
+  }
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = "";
+}
